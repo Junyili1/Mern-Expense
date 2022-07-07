7ed868aee20d78410423288ee66803473f29d75a
Apples <junyiappleli@gmail.com>
2022-07-07 16:39:00 -0400

    protection for user routes

--- backend/controller/userController.js
+++ backend/controller/userController.js
@@ -69,7 +69,12 @@ const loginUser = asyncHandler(async (req, res) => {
 //Gets User
 //Get /api/users/me
 const getUser = asyncHandler(async (req, res) => {
-  res.json({ message: "Fetch User" });
+  const { _id, name, email } = await User.findById(req.user.id);
+  res.status(200).json({
+    id: _id,
+    name,
+    email,
+  });
 });
 
 //Generate JWT Token

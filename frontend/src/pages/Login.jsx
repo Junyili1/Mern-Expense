99f7e28fce904548b5f7f8dedccce8b3a14ed88a
Apples <junyiappleli@gmail.com>
2022-07-19 17:41:21 -0400

    simple header

---
+++ frontend/src/pages/Login.jsx
@@ -0,0 +1,5 @@
+function Dashboard() {
+  return <div>Login</div>;
+}
+
+export default Dashboard;
---
+++ frontend/src/pages/Register.jsx
@@ -0,0 +1,5 @@
+function Dashboard() {
+  return <div>Register</div>;
+}
+
+export default Dashboard;
